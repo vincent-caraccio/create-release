7122b35f301fd5cc079671df638ff69b2251202c
vincent-caraccio <vincent.caraccio@nibbio.io>
2022-11-03 15:02:33 +0800

    bugfix getting context

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const core = require('@actions/core');
-const { github, context } = require('@actions/github');
+const github = require('@actions/github');
 
 (async function () {
   try {
     const token = core.getInput('token');
     const tag_name = core.getInput('tag_name');
     const name = core.getInput('release_name');
-    const { owner, repo } = context.repo;
+    const { owner, repo } = github.context.repo;
     const octokit = github.getOctokit(token);
     const { data } = await octokit.request('POST /repos/{owner}/{repo}/releases', { owner, repo, tag_name, name });
     exportDataToOutput(data);
